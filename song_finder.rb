80f4e58ad24174dd934254ba7fead1a0185afaeb
James Coglan <jcoglan@gmail.com>
2010-09-04 22:52:35 +0100

    Web-scale super-secret web service for Songkick setlist searches.

---
+++ song_finder.rb
@@ -0,0 +1,52 @@
+# Special secret supporting web service, to be run
+# on Songkick's web-scale MongoDB infrastructures.
+
+module Gramophone
+  class SongFinder
+    
+    def call(env)
+      params = Rack::Request.new(env).params
+      
+      venues = Venue.find(:all, :conditions => [
+                                'glat > ? and glat < ? and glng > ? and glng < ?',
+                                params['min_lat'], params['max_lat'],
+                                params['min_lng'], params['max_lng']
+                         ])
+      
+      start_date = Date.new(params['year'].to_i, params['month'].to_i, 1)
+      end_date   = start_date.end_of_month
+      
+      events = venues.inject([]) do |list, venue|
+        list + venue.events.find(:all, :conditions => [
+                                       'date >= ? and date <= ?',
+                                       start_date.strftime('%Y%m%d'),
+                                       end_date.strftime('%Y%m%d')
+                                ])
+      end
+      
+      setlist_items = events.inject([]) do |list, event|
+        event.performances.each do |performance|
+          next unless setlist = performance.setlist
+          setlist.setlist_items.each do |item|
+            list << {
+              :artist => performance.artist.name,
+              :song   => item.name,
+              :venue  => event.venue.name,
+              :lat    => event.venue.glat.to_f,
+              :lng    => event.venue.glng.to_f
+            }
+          end
+        end
+        list
+      end
+      
+      response = params['jsonp'] + '(' + JSON.unparse(setlist_items) + ');'
+      [200, {'Content-type' => 'text/javascript'}, [response]]
+    end
+    
+  end
+end
+
+puts "Booting ..."
+Rack::Handler.get('mongrel').run(Gramophone::SongFinder.new, :Port => 3001)
+
